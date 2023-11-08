4de6c625f08e4edb1900c46ef8e89e27fae8fd93
pasqualecicinelli <pasquale.cicinelli89@gmail.com>
2023-11-08 20:52:05 +0100

    add TypeController

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\ProjectController;
+use App\Http\Controllers\Api\TypeController;
 
 
 /*
@@ -18,6 +19,10 @@ use App\Http\Controllers\Api\ProjectController;
 
 Route::apiResource("/projects", ProjectController::class)->only(["index", "show"]);
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::get("/projects-by-type/{type_id}", [ProjectController::class, 'projectsByType']);
+
+Route::apiResource("types", TypeController::class)->only(["show"]);
+
+//Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//  return $request->user();
+//});
